864d7d8f828a19fc709d8b5d20592683412c79f6
yinkusadel <adeleyeolayinka73@gmail.com>
2023-10-29 22:59:45 +0100

    refactor:mobile menu pop up js

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+const openMenuBtn = document.querySelector('.open-menu-svg');
+const mdHeaderNav = document.querySelector('.small-header-nav');
+const mdHeaderNavLinks = document.querySelectorAll('.md-header-nav-li');
+
+const PopUpCardContainer = document.querySelector('.popup-card-container');
+const seeProjectTonicBtn = document.querySelector('.tonic-popup');
+const seeProjectTonicTwoBtn = document.querySelector('.tonic-two-popup');
+const multiPostPopup = document.querySelector('.multipost-popup');
+const multiPostTwoPopup = document.querySelector('.multipost-two-popup');
+const faceBookPopup = document.querySelector('.facebook-popup');
+const naturePopup = document.querySelector('.nature-popup');
+
+const popUpCardImage = document.querySelector('.popup-card-image');
+const popUpCardCloseBtn = document.querySelector('.popup-card-close-btn');
+
+// const projectCardImages = [
+//   'images/tonic.png',
+//   'images/tonic2.png',
+//   'images/multipost.png',
+//   'images/multipost2.png',
+//   'images/facebook.png',
+//   'images/nature.png',
+// ];
+
+const projectCardImages = {
+  zero: 'images/tonic.png',
+  one: 'images/tonic2.png',
+  two: 'images/multipost.png',
+  three: 'images/multipost2.png',
+  four: 'images/facebook.png',
+  five: 'images/nature.png',
+};
+
+popUpCardImage.setAttribute('src', 'images/tonic.png');
+
+// const popupImageTonicPng = " "
+// const popupImageTonicPng2 = " "
+// const popupImageMultiPostPng = " "
+// const popupImageMultiPostPng2 = " "
+// const popupImageFacebookPng = " "
+// const popupImageNaturePng = " "
+
 const openMenuBtnFunc = () => {
   mdHeaderNav.classList.add('d-flex');
   openMenuBtn.classList.add('d-none');
@@ -21,67 +63,43 @@ openMenu.addEventListener('click', openMenuBtnFunc);
 const closeMenu = document.querySelector('.close-menu-svg');
 closeMenu.addEventListener('click', closeMenuBtnFunc);
 
-const projectCardImages = [
-  'images/tonic.png',
-  'images/tonic2.png',
-  'images/multipost.png',
-  'images/multipost2.png',
-  'images/facebook.png',
-  'images/nature.png',
-];
-let imageIndex = 0;
-
-popUpCardImage.setAttribute('src', 'images/tonic.png');
-
 popUpCardCloseBtn.addEventListener('click', function () {
   PopUpCardContainer.classList.add('d-none');
   PopUpCardContainer.classList.remove('d-flex');
 });
 
 seeProjectTonicBtn.addEventListener('click', function () {
-  imageIndex = 0;
-
   PopUpCardContainer.classList.remove('d-none');
   PopUpCardContainer.classList.add('d-flex');
-  popUpCardImage.src = projectCardImages[imageIndex];
+  popUpCardImage.src = projectCardImages.zero;
 });
 
 seeProjectTonicTwoBtn.addEventListener('click', function () {
-  imageIndex = 1;
-
   PopUpCardContainer.classList.remove('d-none');
   PopUpCardContainer.classList.add('d-flex');
-  popUpCardImage.src = projectCardImages[imageIndex];
+  popUpCardImage.src = projectCardImages.one;
 });
 
 multiPostPopup.addEventListener('click', function () {
-  imageIndex = 2;
-
   PopUpCardContainer.classList.remove('d-none');
   PopUpCardContainer.classList.add('d-flex');
-  popUpCardImage.src = projectCardImages[imageIndex];
+  popUpCardImage.src = projectCardImages.two;
 });
 
 multiPostTwoPopup.addEventListener('click', function () {
-  imageIndex = 3;
-
   PopUpCardContainer.classList.remove('d-none');
   PopUpCardContainer.classList.add('d-flex');
-  popUpCardImage.src = projectCardImages[imageIndex];
+  popUpCardImage.src = projectCardImages.three;
 });
 
 faceBookPopup.addEventListener('click', function () {
-  imageIndex = 4;
-
   PopUpCardContainer.classList.remove('d-none');
   PopUpCardContainer.classList.add('d-flex');
-  popUpCardImage.src = projectCardImages[imageIndex];
+  popUpCardImage.src = projectCardImages.four;
 });
 
 naturePopup.addEventListener('click', function () {
-  imageIndex = 5;
-
   PopUpCardContainer.classList.remove('d-none');
   PopUpCardContainer.classList.add('d-flex');
-  popUpCardImage.src = projectCardImages[imageIndex];
+  popUpCardImage.src = projectCardImages.five;
 });
