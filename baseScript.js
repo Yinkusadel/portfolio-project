2ad27a8825edba6d48ecf8f477714be1553d7fa8
yinkusadel <adeleyeolayinka73@gmail.com>
2023-10-29 22:27:47 +0100

    Feat:add see project popup for mobile menu

---
+++ baseScript.js
@@ -0,0 +1,21 @@
+const openMenuBtn = document.querySelector('.open-menu-svg');
+const mdHeaderNav = document.querySelector('.small-header-nav');
+const mdHeaderNavLinks = document.querySelectorAll('.md-header-nav-li');
+
+const PopUpCardContainer = document.querySelector('.popup-card-container');
+const seeProjectTonicBtn = document.querySelector('.tonic-popup');
+const seeProjectTonicTwoBtn = document.querySelector('.tonic-two-popup');
+const multiPostPopup = document.querySelector('.multipost-popup');
+const multiPostTwoPopup = document.querySelector('.multipost-two-popup');
+const faceBookPopup = document.querySelector('.facebook-popup');
+const naturePopup = document.querySelector('.nature-popup');
+
+const popUpCardImage = document.querySelector('.popup-card-image');
+const popUpCardCloseBtn = document.querySelector('.popup-card-close-btn');
+
+// const popupImageTonicPng = " "
+// const popupImageTonicPng2 = " "
+// const popupImageMultiPostPng = " "
+// const popupImageMultiPostPng2 = " "
+// const popupImageFacebookPng = " "
+// const popupImageNaturePng = " "
--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const openMenuBtn = document.querySelector('.open-menu-svg');
-const mdHeaderNav = document.querySelector('.small-header-nav');
-const mdHeaderNavLinks = document.querySelectorAll('.md-header-nav-li');
-
 const openMenuBtnFunc = () => {
   mdHeaderNav.classList.add('d-flex');
   openMenuBtn.classList.add('d-none');
@@ -24,3 +20,68 @@ openMenu.addEventListener('click', openMenuBtnFunc);
 
 const closeMenu = document.querySelector('.close-menu-svg');
 closeMenu.addEventListener('click', closeMenuBtnFunc);
+
+const projectCardImages = [
+  'images/tonic.png',
+  'images/tonic2.png',
+  'images/multipost.png',
+  'images/multipost2.png',
+  'images/facebook.png',
+  'images/nature.png',
+];
+let imageIndex = 0;
+
+popUpCardImage.setAttribute('src', 'images/tonic.png');
+
+popUpCardCloseBtn.addEventListener('click', function () {
+  PopUpCardContainer.classList.add('d-none');
+  PopUpCardContainer.classList.remove('d-flex');
+});
+
+seeProjectTonicBtn.addEventListener('click', function () {
+  imageIndex = 0;
+
+  PopUpCardContainer.classList.remove('d-none');
+  PopUpCardContainer.classList.add('d-flex');
+  popUpCardImage.src = projectCardImages[imageIndex];
+});
+
+seeProjectTonicTwoBtn.addEventListener('click', function () {
+  imageIndex = 1;
+
+  PopUpCardContainer.classList.remove('d-none');
+  PopUpCardContainer.classList.add('d-flex');
+  popUpCardImage.src = projectCardImages[imageIndex];
+});
+
+multiPostPopup.addEventListener('click', function () {
+  imageIndex = 2;
+
+  PopUpCardContainer.classList.remove('d-none');
+  PopUpCardContainer.classList.add('d-flex');
+  popUpCardImage.src = projectCardImages[imageIndex];
+});
+
+multiPostTwoPopup.addEventListener('click', function () {
+  imageIndex = 3;
+
+  PopUpCardContainer.classList.remove('d-none');
+  PopUpCardContainer.classList.add('d-flex');
+  popUpCardImage.src = projectCardImages[imageIndex];
+});
+
+faceBookPopup.addEventListener('click', function () {
+  imageIndex = 4;
+
+  PopUpCardContainer.classList.remove('d-none');
+  PopUpCardContainer.classList.add('d-flex');
+  popUpCardImage.src = projectCardImages[imageIndex];
+});
+
+naturePopup.addEventListener('click', function () {
+  imageIndex = 5;
+
+  PopUpCardContainer.classList.remove('d-none');
+  PopUpCardContainer.classList.add('d-flex');
+  popUpCardImage.src = projectCardImages[imageIndex];
+});
