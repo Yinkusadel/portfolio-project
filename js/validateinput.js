22fd26f171b5b5f63f211a198211bf18f578c5fb
yinkusadel <adeleyeolayinka73@gmail.com>
2023-12-12 18:17:29 +0100

    refactor: validate contact form

--- js/validateinput.js
+++ js/validateinput.js
@@ -1,9 +1,44 @@
-const validateEmail = () => {
+const validateEmail = (event) => {
   const emailInput = document.getElementById('email').value;
   const errorMessage = document.querySelector('.error-message');
 
-  errorMessage.innerText =
-    emailInput === emailInput.toLowerCase() ? '' : '*Email must be in lowercase.';
+  if (emailInput !== emailInput.toLowerCase()) {
+    errorMessage.innerText = '*Email must be in lowercase.';
+    event.preventDefault();
+  } else {
+    errorMessage.innerText = '';
+  }
+};
+document.querySelector('.contact-get-in-touch-button').addEventListener('click', validateEmail);
+
+const isHidden = (element) => {
+  return element.offsetWidth === 0 && element.offsetHeight === 0;
 };
 
-document.querySelector('.contact-get-in-touch-button').addEventListener('click', validateEmail);
+const validateForm = () => {
+  const inputs = document.querySelectorAll('.customer-contacts-form input');
+  let skipValidation = false;
+
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].type === 'text' && isHidden(inputs[i])) {
+      skipValidation = true;
+      break;
+    }
+
+    if (inputs[i].value.trim() === '') {
+      alert('Please fill in all fields');
+      return false;
+    }
+  }
+
+  if (skipValidation) {
+    return true;
+  }
+
+  alert('Form submitted successfully!');
+  return true;
+};
+
+document.querySelector('.customer-contacts-form').onsubmit = function () {
+  return validateForm();
+};
