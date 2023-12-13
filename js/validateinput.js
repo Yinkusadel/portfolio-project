9d8edb89a305b2c7fa0ccf058a582dd05f1dacac
yinkusadel <adeleyeolayinka73@gmail.com>
2023-12-13 13:58:05 +0100

    feat: update validateinput,js

--- js/validateinput.js
+++ js/validateinput.js
@@ -1,14 +1,18 @@
+const fillFieldErrorMessage = document.querySelector('.fill-field-error-message');
+
 const validateEmail = (event) => {
   const emailInput = document.getElementById('email').value;
   const errorMessage = document.querySelector('.error-message');
 
   if (emailInput !== emailInput.toLowerCase()) {
     errorMessage.innerText = '*Email must be in lowercase.';
+    fillFieldErrorMessage.innerText = '';
     event.preventDefault();
   } else {
     errorMessage.innerText = '';
   }
 };
+
 document.querySelector('.contact-get-in-touch-button').addEventListener('click', validateEmail);
 
 const isHidden = (element) => {
@@ -17,28 +21,24 @@ const isHidden = (element) => {
 
 const validateForm = () => {
   const inputs = document.querySelectorAll('.customer-contacts-form input');
-  let skipValidation = false;
 
   for (let i = 0; i < inputs.length; i += 1) {
     if (inputs[i].type === 'text' && isHidden(inputs[i])) {
-      skipValidation = true;
-      break;
-    }
-
-    if (inputs[i].value.trim() === '') {
-      alert('Please fill in all fields');
-      return false;
+      fillFieldErrorMessage.innerText = '';
+    } else {
+      if (inputs[i].value.trim() === '') {
+        fillFieldErrorMessage.innerText = '*Please fill in all fields.';
+        return false;
+      }
+      fillFieldErrorMessage.innerText = '';
     }
   }
 
-  if (skipValidation) {
-    return true;
-  }
-
-  alert('Form submitted successfully!');
   return true;
 };
 
-document.querySelector('.customer-contacts-form').onsubmit = function () {
-  return validateForm();
-};
+document.querySelector('.customer-contacts-form').addEventListener('submit', (event) => {
+  if (!validateForm()) {
+    event.preventDefault();
+  }
+});
