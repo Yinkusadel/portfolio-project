79caaceb67027c215e51b09007faf41ed24de314
yinkusadel <adeleyeolayinka73@gmail.com>
2023-12-18 20:38:16 +0100

    refactor: validateinput to be more specific and remove required attr from email and text area

--- js/validateinput.js
+++ js/validateinput.js
@@ -1,20 +1,18 @@
-const form = document.getElementById('customer-contacts-form');
-const output = document.getElementById('fill-field-error-message');
-
 const displayErrorMessage = (field, message) => {
   const errorMessage = document.createElement('p');
   errorMessage.classList.add('error-message');
   errorMessage.textContent = message;
 
-  field.insertAdjacentElement('afterend', errorMessage);
+  field.parentNode.insertBefore(errorMessage, field.nextSibling);
 };
+
+const form = document.getElementById('customer-contacts-form');
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
 
   const formData = new FormData(form);
 
-  output.textContent = '';
   document.querySelectorAll('.error-message').forEach((el) => el.remove());
 
   let isValid = true;
@@ -36,30 +34,52 @@ form.addEventListener('submit', function (event) {
     if (key === 'email' && value.trim() !== value.toLowerCase()) {
       displayErrorMessage(field, 'Email should be in lowercase');
       isValid = false;
-    } else if (key === 'fname' && !value.trim() && isFirstNameEmpty && isLastNameEmpty) {
-      if (!isFullNameErrorDisplayed) {
-        displayErrorMessage(field, 'Full name is required');
-        isValid = false;
-        isFullNameErrorDisplayed = true;
-      }
-    } else if (key === 'firstname' && value.trim() && isLastNameEmpty) {
+    } else if (
+      (key === 'fname' || key === 'firstname' || key === 'lastname') &&
+      !value.trim() &&
+      !isFullNameErrorDisplayed
+    ) {
       displayErrorMessage(document.getElementById('fname'), 'Full name is required');
       isValid = false;
-    } else if (key === 'lastname' && value.trim() && isFirstNameEmpty) {
-      displayErrorMessage(document.getElementById('fname'), 'Full name is required');
-      isValid = false;
+      isFullNameErrorDisplayed = true;
     } else if (key !== 'fname' && key !== 'firstname' && key !== 'lastname' && !value.trim()) {
       displayErrorMessage(field, `${key} is required`);
       isValid = false;
       errorMessages.add(`${key} is required`);
-    } else {
-      output.textContent += `${key}: ${value}\n`;
     }
   });
 
   if (isValid) {
-    output.textContent = '';
-  } else {
-    output.textContent = Array.from(errorMessages).join('\n');
+    form.submit();
   }
 });
+
+// let formSubmission = document.getElementById("customer-contacts-form");
+// async function handleSubmit(event) {
+// event.preventDefault();
+// let status = document.getElementById("fill-field-error-message");
+// let data = new FormData(event.target);
+// fetch(event.target.action, {
+//   method: formSubmission.method,
+//   body: data,
+//   headers: {
+//     'Accept': 'application/json'
+// }
+// }).then(response => {
+//   if (response.ok) {
+//     status.innerHTML = "Thanks for your submission!";
+//     formSubmission.reset()
+//   } else {
+//     response.json().then(data => {
+//     if (Object.hasOwn(data, 'errors')) {
+//       status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+//     } else {
+//       status.innerHTML = "Oops! There was a problem submitting your form"
+//     }
+//   })
+// }
+// }).catch(error => {
+//   status.innerHTML = "Oops! There was a problem submitting your form"
+// });
+// }
+// formSubmission.addEventListener("submit", handleSubmit)
