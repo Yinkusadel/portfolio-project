616fd6460bb12fcacd18a5fc8a1102d99190c679
yinkusadel <adeleyeolayinka73@gmail.com>
2024-01-10 20:32:16 +0100

    refactor: local storage so save input on change

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,10 +1,25 @@
-// const form = document.querySelector('form');
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
 
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   const formData = new FormData(form);
+  form.addEventListener('change', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const obj = Object.fromEntries(formData);
 
-//   for (item of formData) {
-//     console.log(item);
-//   }
-// });
+    const json = JSON.stringify(obj);
+    localStorage.setItem('form', json);
+  });
+
+  const storedData = localStorage.getItem('form');
+
+  if (storedData) {
+    const parsedData = JSON.parse(storedData);
+
+    Object.keys(parsedData).forEach((key) => {
+      const inputField = document.querySelector(`[name="${key}"]`);
+      if (inputField) {
+        inputField.value = parsedData[key];
+      }
+    });
+  }
+});
